cfefcafc20b99c21029471ac95816f16ec07ee8b
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-20 20:49:37 +0530

    update

--- main.js
+++ main.js
@@ -253,24 +253,27 @@ document.querySelector('#app').innerHTML = `
     
 
 
-      <a href="https://drive.google.com/file/d/1igNQJvL20r2ICgBsLtLwHYH1oz-ss--3/view?usp=drive_link" 
-   class="cursor-pointer group relative flex gap-1.5 px-8 py-4 bg-black bg-opacity-80 text-[#f1f1f1] rounded-lg hover:bg-opacity-70 transition font-semibold shadow-md">
+      
+    <button class="cursor-pointer group relative flex gap-1.5 px-8 py-4 bg-black bg-opacity-80 text-[#f1f1f1] rounded-lg hover:bg-opacity-70 transition font-semibold shadow-md">
   
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24px" width="24px">
-    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-    <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
-    <g id="SVGRepo_iconCarrier">
-      <g id="Interface / Download">
-        <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#f1f1f1" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" id="Vector"></path>
-      </g>
-    </g>
-  </svg>
-  Resume
-  
-  <div class="absolute opacity-0 -bottom-full rounded-md py-2 px-2 bg-black bg-opacity-70 left-1/2 -translate-x-1/2 group-hover:opacity-100 transition-opacity shadow-lg">
-    Resume
-  </div>
-</a>
+       
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24px" width="24px">
+          <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+          <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
+          <g id="SVGRepo_iconCarrier">
+            <g id="Interface / Download">
+              <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#f1f1f1" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" id="Vector"></path>
+            </g>
+          </g>
+        </svg><a href="https://drive.google.com/file/d/1igNQJvL20r2ICgBsLtLwHYH1oz-ss--3/view?usp=drive_link">
+        Resume</a>
+        <div class="absolute opacity-0 -bottom-full rounded-md py-2 px-2 bg-black bg-opacity-70 left-1/2 -translate-x-1/2 group-hover:opacity-100 transition-opacity shadow-lg">
+          <a href="https://drive.google.com/file/d/1igNQJvL20r2ICgBsLtLwHYH1oz-ss--3/view?usp=drive_link">Resume</a>
+        </div>
+      </button>
+
+      
 
       </div>
   </div>
